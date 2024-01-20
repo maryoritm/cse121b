09846cee9d41446bd850fe149c238b36dbf1af54
maryoritm <maryor.trrs@gmail.com>
2024-01-20 12:41:00 -0600

    f

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -1,38 +1,39 @@
 /* W02-Task - Profile Home Page */
 
-
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-const fullname = "Maryori Torres";
-const currentYear = new Date().getFullYear(); //2024
-const Profilepicture = "images/photo.png";
-
+const fullName = "Maryori Torres";
+const currentYear = new Date().getFullYear(); // 2024
+const profilePicture = "images/photo.png"; 
 
 /* Step 3 - Element Variables */
-
+const nameElement = document.getElementById("name"); 
 const foodElement = document.querySelector('#food');
 const yearElement = document.querySelector("#year");
 const imageElement = document.querySelector('img');
 
-
 /* Step 4 - Adding Content */
-
-
+nameElement.innerHTML = `<strong>${fullName}</strong>`; 
 yearElement.textContent = currentYear;
-imageElement.setAttribute('src', Profilepicture);
-imageElement.setAttribute('alt', `This is ${fullname}. She is....`);
-
-
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `This is ${fullName}. She is`);
 
 /* Step 5 - Array */
-let favfoods = ["Tacos", "Pizza", "Ice cream", "Sushi", "Pasta", "Buergers", "Fruit", "Chicken", "Potato salad", "Steak"];
+let favFoods = ["Tacos", "Pizza", "Ice cream", "Sushi", "Pasta", "Buergers", "Fruit", "Chicken", "Potato salad", "Steak"]; 
 
-foodElement.innerHTML = favfoods;
-let newfood = 'Chocolate';
-favfoods.push(newfood);
-foodElement.innerHTML += `<br>${favfoods}`;
+// Mostrar los alimentos favoritos
+foodElement.innerHTML = favFoods.join(', '); // Úsalo con join para mostrar la lista de alimentos de manera legible
 
+let newFood = 'Chocolate';
+favFoods.push(newFood);
+foodElement.innerHTML += `<br>${favFoods.join(', ')}`; // Añade join para mostrar la lista de alimentos actualizada
 
+// Remover el primer elemento
+favFoods.shift();
+foodElement.innerHTML += `<br>${favFoods.join(', ')}`;
 
+// Remover el último elemento
+favFoods.pop();
+foodElement.innerHTML += `<br>${favFoods.join(', ')}`;
 
